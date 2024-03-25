b9fc219358c8668180ed178070df9bba58beb1b0
trisna.na <trisna.na@gmail.com>
2024-03-25 08:55:14 +0700

    update discounts

--- resources/views/pages/auth/login.blade.php
+++ resources/views/pages/auth/login.blade.php
@@ -1,10 +1,11 @@
 @extends('layouts.auth')
 
-@section('title', 'Login Admin Resto')
+@section('title', 'Login')
 
 @push('style')
     <!-- CSS Libraries -->
-    <link rel="stylesheet" href="{{ asset('library/bootstrap-social/bootstrap-social.css') }}">
+    <link rel="stylesheet"
+        href="{{ asset('library/bootstrap-social/bootstrap-social.css') }}">
 @endpush
 
 @section('main')
@@ -14,7 +15,8 @@
         </div>
 
         <div class="card-body">
-            <form method="POST" action="{{ route('login') }}" class="needs-validation" novalidate="">
+            <form method="POST"
+                action="{{ route('login') }}" class="needs-validation" novalidate="">
                 @csrf
                 <div class="form-group">
                     <label for="email">Email</label>
@@ -32,9 +34,11 @@
 
                 <div class="form-group">
                     <div class="d-block">
-                        <label for="password" class="control-label">Password</label>
+                        <label for="password"
+                            class="control-label">Password</label>
                         <div class="float-right">
-                            <a href="auth-forgot-password.html" class="text-small">
+                            <a href="auth-forgot-password.html"
+                                class="text-small">
                                 Forgot Password?
                             </a>
                         </div>
@@ -51,19 +55,30 @@
                     @enderror
                 </div>
 
+                <div class="form-group">
+                    <div class="custom-control custom-checkbox">
+                        <input type="checkbox"
+                            name="remember"
+                            class="custom-control-input"
+                            tabindex="3"
+                            id="remember-me">
+                        <label class="custom-control-label"
+                            for="remember-me">Remember Me</label>
+                    </div>
+                </div>
 
                 <div class="form-group">
-                    <button type="submit" class="btn btn-primary btn-lg btn-block" tabindex="4">
+                    <button type="submit"
+                        class="btn btn-primary btn-lg btn-block"
+                        tabindex="4">
                         Login
                     </button>
                 </div>
             </form>
-
-
         </div>
     </div>
     <div class="text-muted mt-5 text-center">
-        Don't have an account? <a href="{{ route('register') }}">Create One</a>
+        Don't have an account? <a href="{{ route('register')}}">Create One</a>
     </div>
 @endsection
 
